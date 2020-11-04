cf6f2f58f2264a4d68d450d22c0600034fff37b0
Darrel Miller <darrel.miller@microsoft.com>
2020-11-04 18:32:36 -0500

    Removed scaffold examples

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
 
 import './custom.css'
 import UmlDiagram from './components/UmlDiagram';
@@ -16,6 +14,5 @@ export default () => (
         <Route path='/umldiagram' component={UmlDiagram} />
         <Route path='/openapi' component={OpenApiDescription} />
         <Route path='/swaggerui' component={SwaggerUIView} />
-        <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
     </Layout>
 );
--- ClientApp/src/store/index.ts
+++ ClientApp/src/store/index.ts
@@ -1,11 +1,7 @@
-import * as WeatherForecasts from './WeatherForecasts';
-import * as Counter from './Counter';
 import * as ApiDescription from './ApiDescription';
 
 // The top-level state object
 export interface ApplicationState {
-    counter: Counter.CounterState | undefined;
-    weatherForecasts: WeatherForecasts.WeatherForecastsState | undefined;
     apiDescription: ApiDescription.ApiDescriptionState  | undefined;
 }
 
@@ -13,8 +9,6 @@ export interface ApplicationState {
 // the reducer with the matching name. It's important that the names match exactly, and that the reducer
 // acts on the corresponding ApplicationState property type.
 export const reducers = {
-    counter: Counter.reducer,
-    weatherForecasts: WeatherForecasts.reducer,
     apiDescription: ApiDescription.reducer
 };
 
